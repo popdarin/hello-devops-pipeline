1ab6c0dc87f80ee0e5a3f125117c8606046dd632
Fourchese <darin.nakprawate@gmail.com>
2026-05-31 11:38:43 +0700

    Refactor print statements for clarity in pipeline testing output

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
 print("Hello World - DevOps Pipeline is Running on this channel")
 print("Hello World - DevOps Pipeline testing pipeline is successful")
+print("Hello World - DevOps Pipeline testing round 2 is in progress")
